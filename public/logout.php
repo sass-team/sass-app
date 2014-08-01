0811bf658d823bb5cf89efc75e17e0a072b5198a
Rizart Dokollari <r.dokollari@acg.edu>
2014-08-01 05:00:36 +0300

    Issue: #5: Imporove Logo UI-all.

--- public/logout.php
+++ public/logout.php
@@ -51,7 +51,7 @@ session_destroy();
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     <meta name="description" content="">
     <meta name="author" content=""/>
-    <link rel="shortcut icon" href="<?php echo BASE_URL; ?>app/assets/img/logos/logo-login.png">
+    <link rel="shortcut icon" href="<?php echo BASE_URL; ?>app/assets/img/logos/logo.ico">
 
     <link rel="stylesheet"
           href="http://fonts.googleapis.com/css?family=Open+Sans:400italic,600italic,800italic,400,600,800"
@@ -108,6 +108,6 @@ session_destroy();
 
 <script src="<?php echo BASE_URL; ?>app/assets/js/App.js"></script>
 
-<script src=".<?php echo BASE_URL; ?>app/assets/s/Login.js"></script>
+<script src=".<?php echo BASE_URL; ?>app/assets/js/Login.js"></script>
 </body>
 </html>
