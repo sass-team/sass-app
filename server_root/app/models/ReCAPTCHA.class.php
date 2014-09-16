4972627dfe41982bbf4f6a9d9dcb6b51dcc5c8a3
Rizart Dokollari <r.dokollari@gmail.com>
2014-09-16 17:20:09 +0300

    Fix stage captcha host naming error.

--- server_root/app/models/ReCAPTCHA.class.php
+++ server_root/app/models/ReCAPTCHA.class.php
@@ -12,7 +12,7 @@ class ReCAPTCHA
 	const PUBLIC_KEY_PRODUCTION = "6Lema_oSAAAAADmf0qqidosSIvY0fJJDFaZeSylb";
 	const PRIVATE_KEY_PRODUCTION = "6Lema_oSAAAAAMvkuKrncc7ItGyxMUcDqEXPmvNo";
 
-	const STAGE_HOST = "sass.hol.es";
+	const STAGE_HOST = "dev-sass.hol.es";
 	const PUBLIC_KEY_STAGE = "6Lf0bPoSAAAAAEKxZ8WB9OxakwBS2ZMM1N8ptIDD";
 	const PRIVATE_KEY_STAGE = "6Lf0bPoSAAAAAOKk2ABa7A5Tca5pI65DdiUq3IJI";
 
