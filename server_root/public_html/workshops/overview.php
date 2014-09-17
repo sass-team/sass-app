145202baf9fbd8829ee9385faf51f98b23f7149d
Rizart Dokollari <r.dokollari@gmail.com>
2014-09-17 12:10:13 +0300

    Complete workshops/overview page for admin, secretary & tutor.

--- server_root/public_html/workshops/overview.php
+++ server_root/public_html/workshops/overview.php
@@ -2,6 +2,47 @@
 require __DIR__ . '/../../app/init.php';
 $general->loggedOutProtect();
 
+
+// viewers
+$page_title = "Workshops";
+$section = "workshops";
+
+try {
+	$courses = CourseFetcher::retrieveAll($db);
+
+	if ($user->isTutor()) {
+		// retrieve all appoints for tutor
+		$appointments = TutorFetcher::retrieveAllAppointments($db, $user->getId());
+
+	} else {
+		$terms = TermFetcher::retrieveAll($db);
+		$instructors = InstructorFetcher::retrieveAll($db);
+		$students = StudentFetcher::retrieveAll($db);
+		$tutors = TutorFetcher::retrieveAll($db);
+		$appointments = AppointmentFetcher::retrieveAll($db);
+	}
+
+
+} catch (Exception $e) {
+	$errors[] = $e->getMessage();
+}
+
+
+/**
+ * http://stackoverflow.com/a/4128377/2790481
+ *
+ * @param $findId
+ * @param $objects
+ * @return bool
+ */
+function get($objects, $findId, $column) {
+	foreach ($objects as $object) {
+		if ($object[$column] === $findId) return $object;
+	}
+
+	return false;
+}
+
 // viewers
 $page_title = "Workshops";
 $section = "workshops";
@@ -43,16 +84,33 @@ $section = "workshops";
 
 			<div class="portlet">
 
+				<div class="row">
+					<!-- /.portlet-header -->
 
-				<!-- /.portlet-header -->
+					<?php if (empty($errors) === false): ?>
+						<div class="alert alert-danger">
+							<a class="close" data-dismiss="alert" href="#" aria-hidden="true">×</a>
+							<strong>Oh snap!</strong><?php echo '<p>' . implode('</p><p>', $errors) . '</p>'; ?>
+						</div>
+					<?php endif; ?>
 
+					<div class="col-md-12">
+						<div class="portlet-header">
+							<h3>
+								<i class="fa fa-calendar"></i>
+								Date Picker
+							</h3>
+						</div>
+						<!-- /.portlet-header -->
+						<div class="portlet-content">
 
-				<div id="full-calendar"></div>
+							<div id="workshops-calendar"></div>
+						</div>
 
-
+					</div>
+				</div>
 			</div>
 			<!-- /.portlet -->
-
 		</div>
 		<!-- /#content-container -->
 
@@ -71,47 +129,53 @@ $section = "workshops";
 <?php include ROOT_PATH . "app/views/assets/footer_common.php"; ?>
 
 <!-- dashboard assets -->
-<script src="<?php echo BASE_URL; ?>assets/js/plugins/icheck/jquery.icheck.min.js"></script>
-<script src="<?php echo BASE_URL; ?>assets/js/plugins/tableCheckable/jquery.tableCheckable.js"></script>
+<script src="<?php echo BASE_URL; ?>assets/js/plugins/autosize/jquery.autosize.min.js"></script>
+<script src="<?php echo BASE_URL; ?>assets/js/plugins/textarea-counter/jquery.textarea-counter.js"></script>
+<script src="<?php echo BASE_URL; ?>assets/js/plugins/select2/select2.js"></script>
 
-<script src="<?php echo BASE_URL; ?>assets/js/libs/raphael-2.1.2.min.js"></script>
-<script src="<?php echo BASE_URL; ?>assets/js/plugins/morris/morris.min.js"></script>
 
-<script src="<?php echo BASE_URL; ?>assets/js/demos/charts/morris/area.js"></script>
-<script src="<?php echo BASE_URL; ?>assets/js/demos/charts/morris/donut.js"></script>
-
-<script src="<?php echo BASE_URL; ?>assets/js/plugins/sparkline/jquery.sparkline.min.js"></script>
-
+<script
+	src="<?php echo BASE_URL; ?>assets/js/plugins/bootstrap-datetimepicker/build/js/bootstrap-datetimepicker.min.js">
+</script>
 <script src="<?php echo BASE_URL; ?>assets/js/plugins/fullcalendar/fullcalendar.min.js"></script>
-
-<script src="<?php echo BASE_URL; ?>assets/js/demos/calendar.js"></script>
-
-<script src="<?php echo BASE_URL; ?>assets/js/demos/dashboard.js"></script>
 
 <script type="text/javascript">
 	$(function () {
-		if (!$('#workshop-chart').length) {
-			return false;
-		}
-		workshop();
-		$(window).resize(App.debounce(workshop, 325));
+		moment().format();
+
+		$("#workshops-calendar").fullCalendar({
+			header: {
+				left: 'prev,next',
+				center: 'title',
+				right: 'agendaWeek,month,agendaDay'
+			},
+			weekends: false, // will hide Saturdays and Sundays
+			defaultView: "month",
+			editable: false,
+			droppable: false,
+			events: [
+				<?php	if(sizeof($appointments) <= 1){
+					foreach($appointments as $appointment){
+						$course = get($courses, $appointment[AppointmentFetcher::DB_COLUMN_COURSE_ID], CourseFetcher::DB_COLUMN_ID);
+						include(ROOT_PATH . "app/views/partials/workshops/fullcalendar-single.php");
+					}
+				 }else{
+				   for($i = 0; $i < (sizeof($appointments) - 1); $i++){
+				   $course = get($courses, $appointments[$i][AppointmentFetcher::DB_COLUMN_COURSE_ID], CourseFetcher::DB_COLUMN_ID);
+				      include(ROOT_PATH . "app/views/partials/workshops/fullcalendar-multi.php");
+					}
+					$lastAppointmentIndex = sizeof($appointments)-1;
+					$id = $lastAppointmentIndex;
+					$course = get($courses, $appointments[$i][AppointmentFetcher::DB_COLUMN_COURSE_ID], CourseFetcher::DB_COLUMN_ID);
+					include(ROOT_PATH . "app/views/partials/workshops/fullcalendar-multi.php");
+
+				}
+				?>
+			],
+			timeFormat: 'H(:mm)' // uppercase H for 24-hour clock
+		});
+
 	});
 
-	function workshop() {
-		$('#workshop-chart').empty();
 
-		Morris.Donut({
-			element: 'workshop-chart',
-			data: [
-				{label: 'Successful', value: 60 },
-				{label: 'Canceled', value: 30 },
-				{label: 'Unkown', value: 10 }
-			],
-			colors: ['#0BA462', '#f0ad4e', '#888888'],
-			hideHover: true,
-			formatter: function (y) {
-				return y + "%"
-			}
-		});
-	}
 </script>
