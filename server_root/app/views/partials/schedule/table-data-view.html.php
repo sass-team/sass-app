cd13a39346fba8356748a9ec2d752358e04dc85a
Rizart Dokollari <r.dokollari@gmail.com>
2014-09-19 13:26:49 +0300

    Show weekdays & hours on schedule.

--- server_root/app/views/partials/schedule/table-data-view.html.php
+++ server_root/app/views/partials/schedule/table-data-view.html.php
@@ -1,8 +1,15 @@
+<?php
+$weekTimeStart = new DateTime($schedule[ScheduleFetcher::DB_COLUMN_START_TIME]);
+$weekTimeStart = $weekTimeStart->format("l g:ia");
+$weekTimeEnd = new DateTime($schedule[ScheduleFetcher::DB_COLUMN_END_TIME]);
+$weekTimeEnd = $weekTimeEnd->format("l g:ia");
+
+?>
 <tr>
 	<td class="text-center"><?php echo htmlentities($schedule[UserFetcher::DB_COLUMN_FIRST_NAME]); ?></td>
 	<td class="text-center"><?php echo htmlentities($schedule[UserFetcher::DB_COLUMN_LAST_NAME]); ?></td>
-	<td class="text-center"><?php echo $schedule[ScheduleFetcher::DB_COLUMN_START_TIME]; ?></td>
-	<td class="text-center"><?php echo $schedule[ScheduleFetcher::DB_COLUMN_END_TIME]; ?></td>
+	<td class="text-center"><?php echo $weekTimeStart; ?></td>
+	<td class="text-center"><?php echo $weekTimeEnd; ?></td>
 
 	<td class="text-center">
 		<a href="#updateCourse" data-toggle="modal" class="btn btn-xs btn-primary btnUpdateCourse"><i
