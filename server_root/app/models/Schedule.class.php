0efae0aedd69270586134d56662473e8eeb872a4
Rizart Dokollari <r.dokollari@gmail.com>
2014-09-19 17:23:50 +0300

    Fix bug: staff/schedules creation: malformed dates.

--- server_root/app/models/Schedule.class.php
+++ server_root/app/models/Schedule.class.php
@@ -13,16 +13,18 @@ class Schedule
 		Tutor::validateId($db, $tutorId);
 		Term::validateId($db, $termId);
 
-		self::validateDates($db, $dateStart, $dateEnd, $tutorId);
+		$dateStart = self::validateDate($db, $dateStart, $tutorId);
+		$dateEnd = self::validateDate($db, $dateEnd, $tutorId);
+
 		$appointmentId = ScheduleFetcher::insert($db, $dateStart, $dateEnd, $tutorId, $termId);
 		// TODO: add option for admin to check if he wants an automatic email to be said on said user on his email
 	}
 
-	public static function validateDates($db, $dateStart, $dateEnd, $tutorId) {
-		$dateStart = Dates::initDateTime($dateStart);
-		$dateEnd = Dates::initDateTime($dateEnd);
+	public static function validateDate($db, $date, $tutorId) {
+		$date = Dates::initDateTime($date);
 //		if (ScheduleFetcher::existDatesBetween($db, $dateStart, $dateEnd, $tutorId)) {
 //			throw new Exception("Sorry, the days/hours you inputted conflict with existing working hours.");
 //		}
+		return $date;
 	}
 } 
