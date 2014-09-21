7e3978e1ec8fdcd55e0351459c53f0eddc2ba27c
Rizart Dokollari <r.dokollari@gmail.com>
2014-09-21 21:51:27 +0300

    Remove all paginations.

--- server_root/public_html/staff/index.php
+++ server_root/public_html/staff/index.php
@@ -110,325 +110,325 @@ $section = "staff";
 <?php require ROOT_PATH . 'app/views/head.php'; ?>
 <body>
 <div id="wrapper">
-	<?php
-	require ROOT_PATH . 'app/views/header.php';
-	require ROOT_PATH . 'app/views/sidebar.php';
-	?>
+<?php
+require ROOT_PATH . 'app/views/header.php';
+require ROOT_PATH . 'app/views/sidebar.php';
+?>
 
 
 
-	<div id="content">
+<div id="content">
 
-		<div id="content-header">
-			<h1><?php echo $pageTitle; ?></h1>
-		</div>
-		<!-- #content-header -->
+<div id="content-header">
+	<h1><?php echo $pageTitle; ?></h1>
+</div>
+<!-- #content-header -->
 
 
-		<?php if (!empty($userId)): ?>
-			<div id="content-container">
+<?php if (!empty($userId)): ?>
+	<div id="content-container">
 
+
+		<div class="row">
+
+			<div class="col-md-9">
 
 				<div class="row">
 
-					<div class="col-md-9">
+					<div class="col-md-4 col-sm-5">
 
-						<div class="row">
-
-							<div class="col-md-4 col-sm-5">
-
-								<div class="thumbnail">
-									<img src="<?php echo BASE_URL . $curUser->getAvatarImgLoc(); ?>"
-									     alt="Profile Picture"/>
-								</div>
-								<!-- /.thumbnail -->
-
-								<br/>
-
-							</div>
-							<!-- /.col -->
-
-
-							<div class="col-md-8 col-sm-7">
-
-								<h2><?php echo $curUser->getFirstName() . " " . $curUser->getLastName(); ?></h2>
-
-								<h4>Position: <?php echo ucfirst($curUser->getUserType()) ?></h4>
-
-								<hr/>
-
-
-								<ul class="icons-list">
-									<li><i class="icon-li fa fa-envelope"></i> <?php echo $curUser->getEmail(); ?></li>
-									<li><i class="icon-li fa fa-phone"></i>Mobile: <?php echo $curUser->getMobileNum() ?></li>
-								</ul>
-								<?php if ($curUser->isTutor()) { ?>
-
-									Major: <strong><?php echo $curUser->getMajorId(); ?></strong>
-
-								<?php } ?>
-								<br/>
-								<br/>
-
-								<h3>About me</h3>
-
-								<p><?php echo $curUser->getProfileDescription() ?></p>
-
-								<hr/>
-
-								<br/>
-
-							</div>
-
+						<div class="thumbnail">
+							<img src="<?php echo BASE_URL . $curUser->getAvatarImgLoc(); ?>"
+							     alt="Profile Picture"/>
 						</div>
+						<!-- /.thumbnail -->
+
+						<br/>
+
+					</div>
+					<!-- /.col -->
+
+
+					<div class="col-md-8 col-sm-7">
+
+						<h2><?php echo $curUser->getFirstName() . " " . $curUser->getLastName(); ?></h2>
+
+						<h4>Position: <?php echo ucfirst($curUser->getUserType()) ?></h4>
+
+						<hr/>
+
+
+						<ul class="icons-list">
+							<li><i class="icon-li fa fa-envelope"></i> <?php echo $curUser->getEmail(); ?></li>
+							<li><i class="icon-li fa fa-phone"></i>Mobile: <?php echo $curUser->getMobileNum() ?></li>
+						</ul>
+						<?php if ($curUser->isTutor()) { ?>
+
+							Major: <strong><?php echo $curUser->getMajorId(); ?></strong>
+
+						<?php } ?>
+						<br/>
+						<br/>
+
+						<h3>About me</h3>
+
+						<p><?php echo $curUser->getProfileDescription() ?></p>
+
+						<hr/>
+
+						<br/>
 
 					</div>
 
 				</div>
+
+			</div>
+
+		</div>
+		<!-- /.row -->
+
+		<?php if (!$user->isTutor()): ?>
+			<?php if ($curUser->isTutor()): ?>
+
 				<!-- /.row -->
+				<div class="row">
 
-<?php if (!$user->isTutor()): ?>
-<?php if ($curUser->isTutor()): ?>
-
-	<!-- /.row -->
-	<div class="row">
-
-		<div class="col-md-10">
-			<h3 class="heading">Special Information</h3>
+					<div class="col-md-10">
+						<h3 class="heading">Special Information</h3>
 
 
-			<div class="panel-group accordion" id="accordion">
+						<div class="panel-group accordion" id="accordion">
 
-				<div class="panel panel-default">
-					<div class="panel-heading">
-						<h4 class="panel-title">
-							<a class="accordion-toggle" data-toggle="collapse" data-parent=".accordion"
-							   href="#collapseOne">
-								<i class="fa fa-book"></i>
-								<?php
-								if (empty($teachingCourses)){
-									echo 'The list of Teaching Courses of the current term is empty!';
-								}else{
-									echo 'Current Teaching Courses - ' . $teachingCourses[0][TermFetcher::DB_TABLE . "_" . TermFetcher::DB_COLUMN_NAME];
-								}
-								?>
-							</a>
-						</h4>
-					</div>
+							<div class="panel panel-default">
+								<div class="panel-heading">
+									<h4 class="panel-title">
+										<a class="accordion-toggle" data-toggle="collapse" data-parent=".accordion"
+										   href="#collapseOne">
+											<i class="fa fa-book"></i>
+											<?php
+											if (empty($teachingCourses)) {
+												echo 'The list of Teaching Courses of the current term is empty!';
+											} else {
+												echo 'Current Teaching Courses - ' . $teachingCourses[0][TermFetcher::DB_TABLE . "_" . TermFetcher::DB_COLUMN_NAME];
+											}
+											?>
+										</a>
+									</h4>
+								</div>
 
-					<div id="collapseOne" class="panel-collapse collapse in">
-						<div class="panel-body">
-							<table class="table table-hover">
-								<thead>
-								<tr>
-									<th class="text-center">#</th>
-									<th class="text-center">Course Code</th>
-									<th class="text-center">Course Name</th>
-									<th class="text-center">Status</th>
-								</tr>
-								</thead>
-								<tbody>
-
-								<?php
-								if (empty($errors) === true) {
-									$counter = 1;
-									foreach ($teachingCourses as $course) {
-										include(ROOT_PATH . "app/views/partials/course/table-data-profile-view.html.php");
-										$counter = $counter + 1;
-									}
-								} ?>
-								</tbody>
-							</table>
-						</div>
-					</div>
-					<!-- #collapseOne -->
-				</div>
-				<!-- /.panel-default -->
-
-				<div class="panel panel-default">
-					<div class="panel-heading">
-						<h4 class="panel-title">
-							<a class="accordion-toggle" data-toggle="collapse" data-parent=".accordion"
-							   href="#collapseTwo">
-								<i class="fa fa-clock-o"></i> Current Schedule
-							</a>
-						</h4>
-					</div>
-
-					<div id="collapseTwo" class="panel-collapse collapse">
-						<div class="panel-body">
-							<div class="row">
-
-								<div class="col-md-3">
-
-									<div class="table-responsive">
+								<div id="collapseOne" class="panel-collapse collapse in">
+									<div class="panel-body">
 										<table class="table table-hover">
-
 											<thead>
 											<tr>
-												<th class="text-center" data-filterable="true" data-sortable="true"
-												    data-sortable="true">Starting time
-												</th>
-												<th class="text-center" data-filterable="true" data-sortable="false"
-												    data-sortable="true">Ending time
-
+												<th class="text-center">#</th>
+												<th class="text-center">Course Code</th>
+												<th class="text-center">Course Name</th>
+												<th class="text-center">Status</th>
 											</tr>
 											</thead>
 											<tbody>
 
 											<?php
 											if (empty($errors) === true) {
-												foreach (array_reverse($schedules) as $schedule) {
-													include(ROOT_PATH . "app/views/partials/schedule/profile-table-data-view.html.php");
+												$counter = 1;
+												foreach ($teachingCourses as $course) {
+													include(ROOT_PATH . "app/views/partials/course/table-data-profile-view.html.php");
+													$counter = $counter + 1;
 												}
-											}
-											?>
+											} ?>
 											</tbody>
 										</table>
 									</div>
-									<!-- /.table-responsive -->
-
-
 								</div>
-								<!-- /.col -->
-								<div class="col-md-9">
-									<div class="portlet-header">
+								<!-- #collapseOne -->
+							</div>
+							<!-- /.panel-default -->
+
+							<div class="panel panel-default">
+								<div class="panel-heading">
+									<h4 class="panel-title">
+										<a class="accordion-toggle" data-toggle="collapse" data-parent=".accordion"
+										   href="#collapseTwo">
+											<i class="fa fa-clock-o"></i> Current Schedule
+										</a>
+									</h4>
+								</div>
+
+								<div id="collapseTwo" class="panel-collapse collapse">
+									<div class="panel-body">
+										<div class="row">
+
+											<div class="col-md-3">
+
+												<div class="table-responsive">
+													<table class="table table-hover">
+
+														<thead>
+														<tr>
+															<th class="text-center" data-filterable="true" data-sortable="true"
+															    data-sortable="true">Starting time
+															</th>
+															<th class="text-center" data-filterable="true" data-sortable="false"
+															    data-sortable="true">Ending time
+
+														</tr>
+														</thead>
+														<tbody>
+
+														<?php
+														if (empty($errors) === true) {
+															foreach (array_reverse($schedules) as $schedule) {
+																include(ROOT_PATH . "app/views/partials/schedule/profile-table-data-view.html.php");
+															}
+														}
+														?>
+														</tbody>
+													</table>
+												</div>
+												<!-- /.table-responsive -->
+
+
+											</div>
+											<!-- /.col -->
+											<div class="col-md-9">
+												<div class="portlet-header">
+
+												</div>
+												<!-- /.portlet-header -->
+
+												<div class="portlet-content">
+
+													<div id="appointments-calendar"></div>
+												</div>
+											</div>
+										</div>
+										<!-- /.row -->
 
 									</div>
-									<!-- /.portlet-header -->
-
-									<div class="portlet-content">
-
-										<div id="appointments-calendar"></div>
-									</div>
+									<!-- /.panel-default -->
 								</div>
+								<!-- #collapseTwo -->
 							</div>
-							<!-- /.row -->
-
+							<!-- /.panel-default -->
 						</div>
-						<!-- /.panel-default -->
+						<!-- /.accordion -->
 					</div>
-					<!-- #collapseTwo -->
-				</div>
-				<!-- /.panel-default -->
-			</div>
-			<!-- /.accordion -->
-		</div>
-
-	</div>
-<?php endif; ?>
-<?php endif; ?>
-
-			</div>
-			<!-- /#content-container -->
-
-		<?php
-		// show all users
-		else: ?>
-
-			<div id="content-container">
-				<?php
-				if (empty($errors) === false) {
-					?>
-					<div class="alert alert-danger">
-						<a class="close" data-dismiss="alert" href="#" aria-hidden="true">×</a>
-						<strong>Oh snap!</strong><?php echo '<p>' . implode('</p><p>', $errors) . '</p>'; ?>
-					</div>
-				<?php
-				} ?>
-				<div class="row">
-
-					<div class="col-md-12">
-
-						<div class="portlet">
-
-							<div class="portlet-header">
-
-								<h3>
-									<i class="fa fa-table"></i>
-									Users
-								</h3>
-
-							</div>
-							<!-- /.portlet-header -->
-
-							<div class="portlet-content">
-								<div class="table-responsive">
-									<table
-										class="table table-striped table-bordered table-hover table-highlight table-checkable"
-										data-provide="datatable"
-										data-display-rows="10"
-										data-info="true"
-										data-search="true"
-										data-length-change="true"
-										data-paginate="true"
-										id="usersTable"
-										>
-										<thead>
-										<tr>
-											<th class="text-center" data-filterable="true" data-sortable="true"
-											    data-direction="desc">
-												Name
-											</th>
-											<th class="text-center" data-direction="asc" data-filterable="true"
-											    data-sortable="false">
-												Email
-											</th>
-											<th class="text-center" data-filterable="true" data-sortable="true">Position</th>
-											<th class="text-center" data-filterable="true" data-sortable="false">Mobile</th>
-											<th class="text-center" data-filterable="false" class="hidden-xs hidden-sm">Profile
-											</th>
-
-											<?php if (!$user->isTutor()): ?>
-												<th class="text-center" data-filterable="false" class="hidden-xs hidden-sm">Teaching
-												</th>
-											<?php endif; ?>
-
-											<?php if (!$user->isTutor()): ?>
-												<th class="text-center" data-filterable="false" class="hidden-xs hidden-sm">Schedule
-												</th>
-											<?php endif; ?>
-
-
-											<?php if ($user->isAdmin()): ?>
-												<th class="text-center" data-filterable="false" class="hidden-xs hidden-sm">Data
-												</th>
-											<?php endif; ?>
-										</tr>
-										</thead>
-										<tbody>
-
-										<?php
-										if (empty($errors) === true) {
-											foreach (array_reverse($users) as $curUser) {
-												include(ROOT_PATH . "app/views/partials/user/all-table-data-view.html.php");
-											}
-										} ?>
-										</tbody>
-									</table>
-								</div>
-								<!-- /.table-responsive -->
-
-							</div>
-							<!-- /.portlet-content -->
-
-
-						</div>
-						<!-- /.portlet -->
-
-					</div>
-					<!-- /.col -->
 
 				</div>
-				<!-- /.row -->
-
-
-			</div>
+			<?php endif; ?>
 		<?php endif; ?>
-	</div>
-	<!-- #content -->
 
-	<?php include ROOT_PATH . "app/views/footer.php"; ?>
+	</div>
+	<!-- /#content-container -->
+
+<?php
+// show all users
+else: ?>
+
+	<div id="content-container">
+		<?php
+		if (empty($errors) === false) {
+			?>
+			<div class="alert alert-danger">
+				<a class="close" data-dismiss="alert" href="#" aria-hidden="true">×</a>
+				<strong>Oh snap!</strong><?php echo '<p>' . implode('</p><p>', $errors) . '</p>'; ?>
+			</div>
+		<?php
+		} ?>
+		<div class="row">
+
+			<div class="col-md-12">
+
+				<div class="portlet">
+
+					<div class="portlet-header">
+
+						<h3>
+							<i class="fa fa-table"></i>
+							Users
+						</h3>
+
+					</div>
+					<!-- /.portlet-header -->
+
+					<div class="portlet-content">
+						<div class="table-responsive">
+							<table
+								class="table table-striped table-bordered table-hover table-highlight table-checkable"
+								data-provide="datatable"
+								data-display-rows="10"
+								data-info="true"
+								data-search="true"
+								data-length-change="true"
+								data-paginate="false"
+								id="usersTable"
+								>
+								<thead>
+								<tr>
+									<th class="text-center" data-filterable="true" data-sortable="true"
+									    data-direction="desc">
+										Name
+									</th>
+									<th class="text-center" data-direction="asc" data-filterable="true"
+									    data-sortable="false">
+										Email
+									</th>
+									<th class="text-center" data-filterable="true" data-sortable="true">Position</th>
+									<th class="text-center" data-filterable="true" data-sortable="false">Mobile</th>
+									<th class="text-center" data-filterable="false" class="hidden-xs hidden-sm">Profile
+									</th>
+
+									<?php if (!$user->isTutor()): ?>
+										<th class="text-center" data-filterable="false" class="hidden-xs hidden-sm">Teaching
+										</th>
+									<?php endif; ?>
+
+									<?php if (!$user->isTutor()): ?>
+										<th class="text-center" data-filterable="false" class="hidden-xs hidden-sm">Schedule
+										</th>
+									<?php endif; ?>
+
+
+									<?php if ($user->isAdmin()): ?>
+										<th class="text-center" data-filterable="false" class="hidden-xs hidden-sm">Data
+										</th>
+									<?php endif; ?>
+								</tr>
+								</thead>
+								<tbody>
+
+								<?php
+								if (empty($errors) === true) {
+									foreach (array_reverse($users) as $curUser) {
+										include(ROOT_PATH . "app/views/partials/user/all-table-data-view.html.php");
+									}
+								} ?>
+								</tbody>
+							</table>
+						</div>
+						<!-- /.table-responsive -->
+
+					</div>
+					<!-- /.portlet-content -->
+
+
+				</div>
+				<!-- /.portlet -->
+
+			</div>
+			<!-- /.col -->
+
+		</div>
+		<!-- /.row -->
+
+
+	</div>
+<?php endif; ?>
+</div>
+<!-- #content -->
+
+<?php include ROOT_PATH . "app/views/footer.php"; ?>
 
 </div>
 <!-- #wrapper -->
