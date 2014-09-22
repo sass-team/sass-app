ccbaacd819eae817746f10f4a15167b222986d07
unknown <r.dokollari@gmail.com>
2014-09-22 13:47:50 +0300

    Use static method for api.

--- server_root/app/models/Appointment.class.php
+++ server_root/app/models/Appointment.class.php
@@ -9,42 +9,86 @@
 class Appointment
 {
 
-	public static function add($db, $dateStart, $dateEnd, $courseId, $studentsIds, $tutorId, $instructorsIds, $termId) {
-		$dateStart = Dates::initDateTime($dateStart);
-		$dateEnd = Dates::initDateTime($dateEnd);
-		Course::validateId($db, $courseId);
+    public static function add($db, $dateStart, $dateEnd, $courseId, $studentsIds, $tutorId, $instructorsIds, $termId)
+    {
+        $dateStart = Dates::initDateTime($dateStart);
+        $dateEnd = Dates::initDateTime($dateEnd);
+        Course::validateId($db, $courseId);
 
-		if (sizeof($studentsIds) != sizeof($instructorsIds)) {
-			throw new Exception("An instructor is required for each student.");
-		}
+        if (sizeof($studentsIds) != sizeof($instructorsIds)) {
+            throw new Exception("An instructor is required for each student.");
+        }
 
-		Student::validateIds($db, $studentsIds);
-		Instructor::validateIds($db, $instructorsIds);
-		Tutor::validateId($db, $tutorId);
-		Term::validateId($db, $termId);
+        Student::validateIds($db, $studentsIds);
+        Instructor::validateIds($db, $instructorsIds);
+        Tutor::validateId($db, $tutorId);
+        Term::validateId($db, $termId);
 
-		$appointmentId = AppointmentFetcher::insert($db, $dateStart, $dateEnd, $courseId, $studentsIds, $tutorId, $instructorsIds, $termId);
-		Mailer::sendTutorNewAppointment($db, $appointmentId);
-	}
+        $appointmentId = AppointmentFetcher::insert($db, $dateStart, $dateEnd, $courseId, $studentsIds, $tutorId, $instructorsIds, $termId);
+        Mailer::sendTutorNewAppointment($db, $appointmentId);
+    }
 
-	public static function getSingleTutor($db, $tutorId, $termId) {
-		Tutor::validateId($db, $tutorId);
-		Term::validateId($db, $termId);
-		return AppointmentFetcher::retrieveSingleTutor($db, $tutorId, $termId);
-	}
+    public static function validateId($db, $id)
+    {
+        if (is_null($id) || !preg_match("/^[0-9]+$/", $id)) throw new Exception("Data has been tempered. Aborting process.");
 
-	public static function validateId($db, $id) {
-		if (is_null($id) || !preg_match("/^[0-9]+$/", $id)) throw new Exception("Data has been tempered. Aborting process.");
+        if (!AppointmentFetcher::existsId($db, $id)) {
+            // TODO: sent email to developer relevant to this error.
+            throw new Exception("Either something went wrong with a database query, or you're trying to hack this app. In either case, the developers were just notified about this.");
+        }
+    }
 
-		if (!AppointmentFetcher::existsId($db, $id)) {
-			// TODO: sent email to developer relevant to this error.
-			throw new Exception("Either something went wrong with a database query, or you're trying to hack this app. In either case, the developers were just notified about this.");
-		}
-	}
+    public static function printSingleTutorAppointments($db, $tutorId, $termId)
+    {
+        Tutor::validateId($db, $tutorId);
+        Term::validateId($db, $termId);
+        $appointmentHours = Appointment::getSingleTutor($db, $tutorId, $termId);
+        $appointmentHoursJSON = array();
+        foreach ($appointmentHours as $appointmentHour) {
+            $tutorName = $appointmentHour[CourseFetcher::DB_COLUMN_CODE] . " - " .
+                $appointmentHour[UserFetcher::DB_COLUMN_FIRST_NAME] . " " . $appointmentHour[UserFetcher::DB_COLUMN_LAST_NAME];
+            $startDate = new DateTime($appointmentHour[AppointmentFetcher::DB_COLUMN_START_TIME]);
+            $endDate = new DateTime($appointmentHour[AppointmentFetcher::DB_COLUMN_END_TIME]);
+            $appointmentUrl = "http://" . $_SERVER['SERVER_NAME'] . "/appointments/" . $appointmentHour[UserFetcher::DB_COLUMN_ID];
 
-	public static function getTutorsOnTerm($db, $termId){
-		Term::validateId($db, $termId);
-		return AppointmentFetcher::retrieveTutors($db, $termId);
-	}
+            $appointmentHoursJSON[] = array('title' => $tutorName, 'start' => $startDate->format('Y-m-d H:i:s'), 'end' =>
+                $endDate->format('Y-m-d H:i:s'), 'allDay' => false, 'url' => $appointmentUrl, 'color' => '#e5412d');
+        }
+
+        echo json_encode($appointmentHoursJSON);
+    }
+
+    public static function getSingleTutor($db, $tutorId, $termId)
+    {
+        Tutor::validateId($db, $tutorId);
+        Term::validateId($db, $termId);
+        return AppointmentFetcher::retrieveSingleTutor($db, $tutorId, $termId);
+    }
+
+    public static function  printTutorsAppointments($db, $termId)
+    {
+        Term::validateId($db, $termId);
+        $appointmentHours = Appointment::getTutorsOnTerm($db, $termId);
+        $appointmentHoursJSON = array();
+        foreach ($appointmentHours as $appointmentHour) {
+            $tutorName = $appointmentHour[CourseFetcher::DB_COLUMN_CODE] . " - " .
+                $appointmentHour[UserFetcher::DB_COLUMN_FIRST_NAME] . " " . $appointmentHour[UserFetcher::DB_COLUMN_LAST_NAME];
+            $startDate = new DateTime($appointmentHour[AppointmentFetcher::DB_COLUMN_START_TIME]);
+            $endDate = new DateTime($appointmentHour[AppointmentFetcher::DB_COLUMN_END_TIME]);
+            $appointmentUrl = "http://" . $_SERVER['SERVER_NAME'] . "/appointments/" . $appointmentHour[UserFetcher::DB_COLUMN_ID];
+
+            $appointmentHoursJSON[] = array('title' => $tutorName, 'start' => $startDate->format('Y-m-d H:i:s'), 'end' =>
+                $endDate->format('Y-m-d H:i:s'), 'allDay' => false, 'url' => $appointmentUrl, 'color' => '#e5412d');
+        }
+
+        echo json_encode($appointmentHoursJSON);
+    }
+
+    public static function getTutorsOnTerm($db, $termId)
+    {
+        Term::validateId($db, $termId);
+        return AppointmentFetcher::retrieveTutors($db, $termId);
+    }
+
 
 }
