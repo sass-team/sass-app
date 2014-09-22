5f013e56fb83948d7c238f28d12f057c253d4fdf
unknown <r.dokollari@gmail.com>
2014-09-22 12:59:43 +0300

    Show students name on each appointments.

--- server_root/app/models/AppointmentHasStudentFetcher.class.php
+++ server_root/app/models/AppointmentHasStudentFetcher.class.php
@@ -8,17 +8,18 @@
  */
 class AppointmentHasStudentFetcher
 {
-	const DB_TABLE = "appointment_has_student";
-	const DB_COLUMN_ID = "id";
-	const DB_COLUMN_APPOINTMENT_ID = "appointment_id";
-	const DB_COLUMN_STUDENT_ID = "student_id";
-	const DB_COLUMN_REPORT_ID = "report_id";
-	const DB_COLUMN_INSTRUCTOR_ID = "instructor_id";
+    const DB_TABLE = "appointment_has_student";
+    const DB_COLUMN_ID = "id";
+    const DB_COLUMN_APPOINTMENT_ID = "appointment_id";
+    const DB_COLUMN_STUDENT_ID = "student_id";
+    const DB_COLUMN_REPORT_ID = "report_id";
+    const DB_COLUMN_INSTRUCTOR_ID = "instructor_id";
 
-	public static function insert($db, $appointmentId, $studentId, $instructorId) {
-		try {
-			$queryInsertUser = "INSERT INTO `" . DB_NAME . "`.`" . self::DB_TABLE . "` (`" . self::DB_COLUMN_APPOINTMENT_ID
-				. "`,	`" . self::DB_COLUMN_STUDENT_ID . "`, `" . self::DB_COLUMN_INSTRUCTOR_ID . "`
+    public static function insert($db, $appointmentId, $studentId, $instructorId)
+    {
+        try {
+            $queryInsertUser = "INSERT INTO `" . DB_NAME . "`.`" . self::DB_TABLE . "` (`" . self::DB_COLUMN_APPOINTMENT_ID
+                . "`,	`" . self::DB_COLUMN_STUDENT_ID . "`, `" . self::DB_COLUMN_INSTRUCTOR_ID . "`
 				)
 				VALUES(
 					:appointment_id,
@@ -27,89 +28,120 @@ class AppointmentHasStudentFetcher
 				)";
 
 
-			$queryInsertUser = $db->getConnection()->prepare($queryInsertUser);
-			$queryInsertUser->bindParam(':appointment_id', $appointmentId, PDO::PARAM_STR);
-			$queryInsertUser->bindParam(':student_id', $studentId, PDO::PARAM_STR);
-			$queryInsertUser->bindParam(':instructor_id', $instructorId, PDO::PARAM_STR);
+            $queryInsertUser = $db->getConnection()->prepare($queryInsertUser);
+            $queryInsertUser->bindParam(':appointment_id', $appointmentId, PDO::PARAM_STR);
+            $queryInsertUser->bindParam(':student_id', $studentId, PDO::PARAM_STR);
+            $queryInsertUser->bindParam(':instructor_id', $instructorId, PDO::PARAM_STR);
 
-			$queryInsertUser->execute();
+            $queryInsertUser->execute();
 
-		} catch (Exception $e) {
-			throw new Exception("Could not insert user into database.");
-		}
+        } catch (Exception $e) {
+            throw new Exception("Could not insert user into database.");
+        }
 
-	}
+    }
 
-	public static function update($db, $id, $reportId) {
-		$query = "UPDATE `" . DB_NAME . "`.`" . self::DB_TABLE . "`
+    public static function update($db, $id, $reportId)
+    {
+        $query = "UPDATE `" . DB_NAME . "`.`" . self::DB_TABLE . "`
 					SET `" . self::DB_COLUMN_REPORT_ID . "`= :report_id
 					WHERE `" . self::DB_COLUMN_ID . "` = :appointment_id";
 
-		try {
-			$query = $db->getConnection()->prepare($query);
-			$query->bindParam(':appointment_id', $id, PDO::PARAM_INT);
-			$query->bindParam(':report_id', $reportId, PDO::PARAM_INT);
+        try {
+            $query = $db->getConnection()->prepare($query);
+            $query->bindParam(':appointment_id', $id, PDO::PARAM_INT);
+            $query->bindParam(':report_id', $reportId, PDO::PARAM_INT);
 
-			$query->execute();
+            $query->execute();
 
-			if ($query->rowCount() == 0) {
-				throw new Exception();
-			}
-			return true;
-		} catch (Exception $e) {
-			throw new Exception("Could not update data." . $e->getMessage());
-		}
-		return false;
-	}
+            if ($query->rowCount() == 0) {
+                throw new Exception();
+            }
+            return true;
+        } catch (Exception $e) {
+            throw new Exception("Could not update data." . $e->getMessage());
+        }
+        return false;
+    }
 
-	public static function existsId($db, $id) {
-		try {
-			$sql = "SELECT COUNT(" . self::DB_COLUMN_ID . ") FROM `" . DB_NAME . "`.`" .
-				self::DB_TABLE . "` WHERE `" . self::DB_COLUMN_ID . "` = :id";
-			$query = $db->getConnection()->prepare($sql);
-			$query->bindParam(':id', $id, PDO::PARAM_INT);
-			$query->execute();
+    public static function existsId($db, $id)
+    {
+        try {
+            $sql = "SELECT COUNT(" . self::DB_COLUMN_ID . ") FROM `" . DB_NAME . "`.`" .
+                self::DB_TABLE . "` WHERE `" . self::DB_COLUMN_ID . "` = :id";
+            $query = $db->getConnection()->prepare($sql);
+            $query->bindParam(':id', $id, PDO::PARAM_INT);
+            $query->execute();
 
-			if ($query->fetchColumn() === 0) return false;
-		} catch (Exception $e) {
-			throw new Exception("Could not check if data already exists.");
-		}
+            if ($query->fetchColumn() === 0) return false;
+        } catch (Exception $e) {
+            throw new Exception("Could not check if data already exists.");
+        }
 
-		return true;
-	}
+        return true;
+    }
 
-	public static function retrieveAll($db) {
-		$query =
-			"SELECT `" . self::DB_COLUMN_ID . "` , `" . self::DB_COLUMN_APPOINTMENT_ID . "` , `" . self::DB_COLUMN_STUDENT_ID . "`,
+    public static function retrieveAll($db)
+    {
+        $query =
+            "SELECT `" . self::DB_COLUMN_ID . "` , `" . self::DB_COLUMN_APPOINTMENT_ID . "` , `" . self::DB_COLUMN_STUDENT_ID . "`,
 			 `" . self::DB_COLUMN_REPORT_ID . "`,  `" . self::DB_COLUMN_INSTRUCTOR_ID . "`
 			FROM `" . DB_NAME . "`.`" . self::DB_TABLE . "`";
 
-		try {
-			$query = $db->getConnection()->prepare($query);
-			$query->execute();
+        try {
+            $query = $db->getConnection()->prepare($query);
+            $query->execute();
 
-			return $query->fetchAll(PDO::FETCH_ASSOC);
-		} catch (PDOException $e) {
-			throw new Exception("Could not retrieve data from database.");
-		}
-	}
+            return $query->fetchAll(PDO::FETCH_ASSOC);
+        } catch (PDOException $e) {
+            throw new Exception("Could not retrieve data from database.");
+        }
+    }
 
-	public static function retrieveJoinReport($db, $appointmentId) {
-		$query =
-			"SELECT `" . self::DB_COLUMN_ID . "` , `" . self::DB_COLUMN_APPOINTMENT_ID . "` , `" . self::DB_COLUMN_STUDENT_ID . "`,
+    public static function retrieveJoinReport($db, $appointmentId)
+    {
+        $query =
+            "SELECT `" . self::DB_COLUMN_ID . "` , `" . self::DB_COLUMN_APPOINTMENT_ID . "` , `" . self::DB_COLUMN_STUDENT_ID . "`,
 			 `" . self::DB_COLUMN_REPORT_ID . "`,  `" . self::DB_COLUMN_INSTRUCTOR_ID . "`
 			FROM `" . DB_NAME . "`.`" . self::DB_TABLE . "`
 			WHERE `" . self::DB_TABLE . "`.`" . self::DB_COLUMN_APPOINTMENT_ID . "`=:appointemt_id";
 
-		try {
-			$query = $db->getConnection()->prepare($query);
-			$query->bindParam(':appointemt_id', $appointmentId, PDO::PARAM_INT);
-			$query->execute();
+        try {
+            $query = $db->getConnection()->prepare($query);
+            $query->bindParam(':appointemt_id', $appointmentId, PDO::PARAM_INT);
+            $query->execute();
 
-			return $query->fetchAll(PDO::FETCH_ASSOC);
-		} catch (PDOException $e) {
-			throw new Exception("Could not retrieve data from database." . $e->getMessage());
-		}
-	}
+            return $query->fetchAll(PDO::FETCH_ASSOC);
+        } catch (PDOException $e) {
+            throw new Exception("Could not retrieve data from database." . $e->getMessage());
+        }
+    }
+
+
+    public static function retrieveJoinStudent($db, $appointmentId)
+    {
+        $query =
+            "SELECT `" . self::DB_TABLE . "`.`" . self::DB_COLUMN_ID . "` , `" . self::DB_TABLE . "`.`" .
+            self::DB_COLUMN_APPOINTMENT_ID . "` ,  `" . self::DB_TABLE . "`.`" . self::DB_COLUMN_STUDENT_ID . "`,
+            `" . self::DB_TABLE . "`.`" . self::DB_COLUMN_REPORT_ID . "`,  `" . self::DB_TABLE . "`.`" .
+            self::DB_COLUMN_INSTRUCTOR_ID . "`, `" . StudentFetcher::DB_TABLE . "`.`" .
+            StudentFetcher::DB_COLUMN_FIRST_NAME . "`, `" . StudentFetcher::DB_TABLE . "`.`" .
+            StudentFetcher::DB_COLUMN_LAST_NAME . "`
+			FROM `" . DB_NAME . "`.`" . self::DB_TABLE . "`
+			INNER JOIN  `" . DB_NAME . "`.`" . StudentFetcher::DB_TABLE . "`
+			ON `" . DB_NAME . "`.`" . StudentFetcher::DB_TABLE . "`.`" . StudentFetcher::DB_COLUMN_ID . "`  = `" .
+            self::DB_TABLE . "`.`" . self::DB_COLUMN_STUDENT_ID . "`
+			WHERE `" . self::DB_TABLE . "`.`" . self::DB_COLUMN_APPOINTMENT_ID . "`=:appointemt_id";
+
+        try {
+            $query = $db->getConnection()->prepare($query);
+            $query->bindParam(':appointemt_id', $appointmentId, PDO::PARAM_INT);
+            $query->execute();
+
+            return $query->fetchAll(PDO::FETCH_ASSOC);
+        } catch (PDOException $e) {
+            throw new Exception("Could not retrieve data from database." . $e->getMessage());
+        }
+    }
 
 } 
