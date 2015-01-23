06b544bd3282515c4ca473bfe78822500a277fbd
Rizart Dokollari <r.dokollari@gmail.com>
2015-01-23 05:18:29 +0200

    Remove file Database.class.php

--- public_html/app/config/app.php
+++ public_html/app/config/app.php
@@ -40,9 +40,9 @@ require_once ROOT_PATH . "controllers/App.class.php";
 
 
 if (strcmp(App::PRODUCTION_HOST, $_SERVER['SERVER_NAME']) === 0 && $_SERVER["HTTPS"] != "on") {
-       header("HTTP/1.1 301 Moved Permanently");
-       header("Location: https://" . $_SERVER["SERVER_NAME"] . $_SERVER["REQUEST_URI"]);
-       exit();
+	header("HTTP/1.1 301 Moved Permanently");
+	header("Location: https://" . $_SERVER["SERVER_NAME"] . $_SERVER["REQUEST_URI"]);
+	exit();
 }
 
 require ROOT_PATH . "config/loadAllClasses.php";
