87f92249b3bf0452e1981a05ba2e66d074f30048
Rizart Dokollari <r.dokollari@gmail.com>
2015-02-05 18:06:46 +0200

    Check HTTP_POST server var exists before using

--- public_html/app/config/app.php
+++ public_html/app/config/app.php
@@ -47,7 +47,7 @@ if (!App::isSecure() && isset($_SERVER['SERVER_NAME']) && App::isHostnameInSSLLi
 	exit();
 }
 
-if (substr($_SERVER['HTTP_HOST'], 0, 4) === 'www.')
+if (isset($_SERVER['HTTP_POST']) && (substr($_SERVER['HTTP_HOST'], 0, 4) === 'www.'))
 {
 	header('Location: http' . (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on' ? 's' : '') . '://' . substr($_SERVER['HTTP_HOST'], 4) . $_SERVER['REQUEST_URI']);
 	exit;
