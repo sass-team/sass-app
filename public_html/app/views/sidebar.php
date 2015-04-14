c37333279d76aac6738b00ce458cfc74c02e6eee
Rizart Dokollari <r.dokollari@gmail.com>
2015-04-14 15:58:05 +0300

    Disable search page view.

--- public_html/app/views/sidebar.php
+++ public_html/app/views/sidebar.php
@@ -194,15 +194,6 @@
 			</ul>
 		</li>
 
-		<?php if ($user->isAdmin()) { ?>
-			<li class="<?php if ($section == "search") echo "active"; ?>">
-				<a href="<?php echo BASE_URL; ?>search">
-					<i class="fa fa-search"></i>
-					Search
-				</a>
-			</li>
-
-		<?php } ?>
 
 		<?php if ($user->isAdmin()) { ?>
 			<li class="<?php if ($section == "cloud") echo "active"; ?>">
